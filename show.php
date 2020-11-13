107811ef2218692b8d4a8964d2c8ef6090c21215
rika2222 <68225384+rika2222@users.noreply.github.com>
2020-11-13 22:12:21 +0900

    error fixes

--- show.php
+++ show.php
@@ -5,7 +5,7 @@
         
         $db = parse_url($_SERVER[' mysql://bca9c6ea51e0fc:86030753@eu-cdbr-west-03.cleardb.net/heroku_932d18c23319b85?reconnect=true']);
         $db['dbname'] = ltrim($db['path'], '/');  
-        $dsn = 'mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8';
+        $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
         $username = 'root';
         $password = '';
       
