7f597132e205a650eb5031b2db6422ddba546d32
rika2222 <68225384+rika2222@users.noreply.github.com>
2020-11-13 22:00:26 +0900

    update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '8401c6a3bb2cab2575609be8e390bf9701147ce2',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8401c6a3bb2cab2575609be8e390bf9701147ce2',
+    ),
+  ),
+);
