9504bba27c9c4fee03e9dc215c971338bb297d2d
rika2222 <68225384+rika2222@users.noreply.github.com>
2020-11-13 22:14:44 +0900

    another fix

--- new.php
+++ new.php
@@ -11,7 +11,7 @@
         $tag = $_POST['tag'];
         $url = $_POST['url'];
         
-        $db = parse_url($_SERVER[' mysql://bca9c6ea51e0fc:86030753@eu-cdbr-west-03.cleardb.net/heroku_932d18c23319b85?reconnect=true']);
+        $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
         $db['dbname'] = ltrim($db['path'], '/');  
         $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
         $db_username = 'root';
--- show.php
+++ show.php
@@ -3,7 +3,7 @@
        
         $fragen_id = $_GET['id'];
         
-        $db = parse_url($_SERVER[' mysql://bca9c6ea51e0fc:86030753@eu-cdbr-west-03.cleardb.net/heroku_932d18c23319b85?reconnect=true']);
+        $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
         $db['dbname'] = ltrim($db['path'], '/');  
         $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
         $username = 'root';
