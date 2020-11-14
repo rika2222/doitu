fbfc06e4d17cc0d86b80a374a7cb3147a9d9978c
rika2222 <68225384+rika2222@users.noreply.github.com>
2020-11-14 21:11:13 +0900

    Update index.php

--- index.php
+++ index.php
@@ -274,7 +274,7 @@
                     <ul class="float-right">
                         <div class="row r21">
                             <div class="offset-md-1 col-md-4 foto-DSH">
-                                <a class="foto-DSHa" href="./"><img src="201002testdaf.jpg" alt="foto-testdaf" height="300" width="300"></a>
+                                <a class="foto-DSHa" href="./"><img src="201002testdaf.jpg" alt="foto-testdaf" height="300" width="100%"></a>
                             </div>
                             <div class="col-md-7 TDmerkmsl">
                                 <li class="testdafli">TestDafの特徴</li>
